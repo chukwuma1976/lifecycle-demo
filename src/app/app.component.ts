6258a10b7f8082a1e633cca842ab05edda366006
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-10 13:08:50 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DemoComponent } from "./demo/demo.component";
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DemoComponent, FormsModule, NgIf, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'lifecycle-demo';
+
+  title = 'Lifecycle Demo';
+  hidden = false;
+
+  hideComponent() {
+    this.hidden = !this.hidden
+  }
+
 }
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+export class DemoComponent {
+
+  @Input('title') titleString: string  = '';
+
+  constructor(){
+    console.log('constructor called');
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges called');
+  }
+
+  ngOnInit(){
+    console.log('ngOnInit called');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
+  }
+
+}
